1bfdcf6cb2e5f2ec8bbecf781a59994348bad542
WeiPu456 <67470344+WeiPu456@users.noreply.github.com>
2023-04-25 20:15:05 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+
+import tensorflow as tf
+import os
+import yaml
+import h5py
+
+from Model.Decoder_Handler import Decoder_Handler
+
+config_filename = './Model/Config.yaml'
+scenario = 'NBA'
+
+def main():
+
+    folder_id, config_id = 'NBA-Decoder-TNN', 'config-nba.yaml'
+
+    with open(config_filename) as handle:
+        model_config = yaml.load(handle)
+    log_dir = os.path.join(os.path.abspath('.'), model_config['result_dir'], model_config['result_model'], folder_id)
+
+    with open(os.path.join(log_dir, config_id)) as handle:
+        model_config = yaml.load(handle)
+    data_name = os.path.join(os.path.abspath('.'), 'Data', model_config['category'], model_config['data_name'])
+    if model_config['mask_name'] == 'Original':
+        mask_name = None
+    else:
+        mask_name = os.path.join(os.path.abspath('.'),'Data',model_config['category'],model_config['mask_name'])
+        
+    dataset_name = (data_name,mask_name)
+    
+    tf_config = tf.ConfigProto()
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2" # Please change the id of GPU in your local server accordingly
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+
+    with tf.Session(config=tf_config) as sess:
+        Cube_Decoder = Decoder_Handler(dataset_name=dataset_name, model_config=model_config, sess = sess, is_training=False)
+        Cube_Decoder.test()
+
+if __name__ == '__main__':
+    main()
